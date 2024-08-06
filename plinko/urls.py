71821f83f9a8abcd758352a2f9f321ecb32af36f
anrays2a <91744118+3irna@users.noreply.github.com>
2024-08-06 14:52:12 +0330

    Add files via upload

---
+++ plinko/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from django.views.static import serve
+from home.views import home_page
+from plinko import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
+    path('admin/', admin.site.urls),
+    path('', home_page),
+]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
